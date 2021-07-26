886e66aa65ff7b22edd9ed680fda1689114e360c
bsm7878 <bsm7878@naver.com>
2021-07-26 11:13:21 +0900

    Add data-netlify

--- src/components/rank/speach/Speach.js
+++ src/components/rank/speach/Speach.js
@@ -50,7 +50,7 @@ const Speach = ({uniq_num}) => {
       const {company, username, password, content} = values;
       const data = {company, username, password, content}
       
-      axiosInstance.post("/api/comment/", data, {headers: { "Content-Type": "application/json" }})
+      axiosInstance.post("/api/comment/", data, {headers: { "Content-Type": "application/x-www-form-urlencoded" }})
         .then(response => {
             notification.open({
               message: '작성 완료',
@@ -138,7 +138,7 @@ const Speach = ({uniq_num}) => {
                   okText={'게시'}
                   cancelText={'취소'}
                 >
-                  <Form onFinish={onFinish}>
+                  <Form onFinish={onFinish} data-netlify="true">
                   <div>
                   <Row>
                     <Col xs={{span: 24}} sm={{ span: 11 }}>    
